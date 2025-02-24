46160e78b4d94164c47ff2411442a9eb21eae6d9
ArtichautCosmique <lelolartichaut@laposte.net>
2025-02-24 23:24:01 +0100

    offset ch2 brahms

--- src/music/brahms.c
+++ src/music/brahms.c
@@ -85,73 +85,73 @@ chord_t BRAHMS_ch1[] = {
 chord_t BRAHMS_ch2[] = {
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
-    { 0, NULL , 0 },
-
-    /* --- --- --- */
-
-    { 1, (octnote_t []) { OCTNOTE(RE, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(FA, 4), OCTNOTE(SOL, 4) }, 0 },
-    { 0, NULL , 0 },
-
-    { 1, (octnote_t []) { OCTNOTE(SI, 3) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
-    { 0, NULL , 0 },
-
-    { 1, (octnote_t []) { OCTNOTE(SI, 3) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
-    { 0, NULL , 0 },
-
-    { 1, (octnote_t []) { OCTNOTE(RE, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(FA, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
     /* --- --- --- */
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(RE, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(FA, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(SI, 2) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(SI, 2) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(SI, 3) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(RE, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(FA, 3), OCTNOTE(SOL, 3) }, 0 },
+    { 0, NULL , 0 },
+
+    /* --- --- --- */
+
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
+    { 0, NULL , 0 },
+
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
+    { 0, NULL , 0 },
+
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
+    { 0, NULL , 0 },
+
+    { 1, (octnote_t []) { OCTNOTE(SI, 2) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
     /* --- --- --- */
     
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(DO, 4) }, 0 },
-    { 2, (octnote_t []) { OCTNOTE(MI, 4), OCTNOTE(SOL, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(DO, 3) }, 0 },
+    { 2, (octnote_t []) { OCTNOTE(MI, 3), OCTNOTE(SOL, 3) }, 0 },
     { 0, NULL , 0 },
 
-    { 1, (octnote_t []) { OCTNOTE(LA, 4) }, 0 },
-    { 1, (octnote_t []) { OCTNOTE(SOL, 4) }, 0 },
-    { 1, (octnote_t []) { OCTNOTE(FA, 4) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(LA, 3) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(SOL, 3) }, 0 },
+    { 1, (octnote_t []) { OCTNOTE(FA, 3) }, 0 },
 
-    { 2, (octnote_t []) { OCTNOTE(DO, 4),OCTNOTE(MI, 4) }, 1 },
+    { 2, (octnote_t []) { OCTNOTE(DO, 3),OCTNOTE(MI, 3) }, 1 },
     { 0, NULL , 0 },
 
     END_NOTE
