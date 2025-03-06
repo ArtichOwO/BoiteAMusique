e3d3b3cff33b1b6e94de98138761febc6729d4ef
Haschley <sarah.cordary@polybot-grenoble.fr>
2025-03-06 21:53:37 +0100

    anges.c

--- src/music/anges.c
+++ src/music/anges.c
@@ -1,5 +1,12 @@
 #include <note.h>
 #include <music.h>
+/*Comment sont écrit les notes ?
+	{ nbr_notes_même_temps, (octnote_t []) { OCTNOTE(note, numéro octave) }, duree_note },
+ 	duree_note, 	soit -1 pour les croches,
+  			        soit 0 pour les noires,
+     			    soit 1 pour les blanches,
+	(pour des puissances de 2) 
+*/
 
 chord_t ANGES_ch1[] = {
 // Couplet 1 : "Les anges dans nos campagnes..."
